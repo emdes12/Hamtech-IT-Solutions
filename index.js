f3869b9fe1b92050fb637f0d38bbe3d2ca77452e
Alawiye Muritala <naijabayz@gmail.com>
2023-01-28 18:17:17 +0100

    worked on countdown for review

--- index.js
+++ index.js
@@ -28,4 +28,50 @@ function updateYear() {
   let year = date.getFullYear();
   footYear.innerHTML = `&copy ${year} Tech-Lot`;
 }
-window.onload(updateYear());
+updateYear();
+
+// countdown animation
+const projectEl = document.querySelector(".projects");
+const clientEl = document.querySelector(".clients");
+const staffEl = document.querySelector(".staffs");
+
+let num = 0;
+
+let interval = setInterval(function () {
+  num++;
+
+  if (num < 12) {
+    staffEl.textContent = num;
+  } else {
+    staffEl.textContent = 12;
+  }
+
+  if (num < 120) {
+    clientEl.textContent = num;
+  } else {
+    clientEl.textContent = 120;
+  }
+
+  if (num < 1000) {
+    projectEl.textContent = num;
+  } else {
+    clearInterval(interval);
+    projectEl.textContent = "1.3K";
+  }
+}, 0.3);
+
+window.addEventListener("scroll", () => {
+  let scrollMeasure = window.pageYOffset;
+  const reviewTop = document.querySelector(".list-review").offsetTop;
+  let windowHeight = window.innerHeight;
+  if (
+    scrollMeasure > reviewTop - windowHeight &&
+    scrollMeasure < reviewTop + 100
+  ) {
+    // console.log("on it");
+    interval();
+  } else {
+    // console.log("out of bound");
+    num = 0;
+  }
+});
